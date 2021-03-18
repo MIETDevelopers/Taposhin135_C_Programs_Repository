5e4e6c2cf23b13eb99b6a21bb22d867ce8faecae
Taposhin135pradhan <76278922+Taposhin135pradhan@users.noreply.github.com>
2021-03-18 07:44:36 +0530

    User choice

--- 4_Areauserchoice.c
+++ 4_Areauserchoice.c
@@ -1,4 +1,4 @@
-//To find the area of Circle, Square and Rectangle by user choice
+//To find the area of Circle, Square and Rectangle user choice
 //Area of Circle: (22/7)*r*r: radius
 //Area of Square: side*side: side
 //Area of Rectangle: Length * Breadth: Length, Breadth
@@ -6,7 +6,7 @@
 #include<stdio.h>
 int main(){
     float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;
-
+    
     raduisOfCircle=7;
     sideOfSquare=2;
     legthOfRectangle=2;
@@ -15,5 +15,6 @@ int main(){
     printf("Area of Circle: %f",((22*raduisOfCircle*raduisOfCircle)/7));
     printf("\nArea of Square: %f",(sideOfSquare*sideOfSquare));
     printf("\nArea of Rectangle: %f",(legthOfRectangle*breadthOfRectangle));
+
     return 0;
 }
