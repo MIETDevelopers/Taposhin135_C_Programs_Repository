02f1ec6d2d66477c71083385cd6d9639c428c75b
Taposhin135pradhan <76278922+Taposhin135pradhan@users.noreply.github.com>
2021-03-19 10:26:11 +0530

    Factorial

--- 8_Factorial.c
+++ 8_Factorial.c
@@ -1,13 +1,12 @@
 // Program to calculate Factorial of a number.
 #include<stdio.h>  
-int main()    
-{    
+int main(){    
  int i,fact=1,number;    
  printf("Enter a number: ");    
   scanf("%d",&number); // scan the number entered by the user.   
     for(i=1;i<=number;i++){    
       fact=fact*i;    
-  }    
-  printf("Factorial of %d is: %d",number,fact);    
-return 0;
+    }    
+     printf("Factorial of %d is: %d",number,fact);    
+     return 0;
 }
