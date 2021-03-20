86e104111eabf82011599d82eb3cc2ebea7fad0c
Taposhin135pradhan <76278922+Taposhin135pradhan@users.noreply.github.com>
2021-03-20 11:53:50 +0530

    Sum of digits

---
+++ 8_Factorialnumber.c
@@ -0,0 +1,13 @@
+// Program to calculate Factorial of a number.
+#include<stdio.h>  
+int main()    
+{    
+ int i,fact=1,number;    
+ printf("Enter a number: ");    
+  scanf("%d",&number); // scan the number entered by the user.   
+    for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  printf("Factorial of %d is: %d",number,fact);    
+return 0;
+}
